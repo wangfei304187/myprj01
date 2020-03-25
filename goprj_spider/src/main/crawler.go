be73bb21aa845e590184c725ce3e73ba04047581
root <root@localhost>
2020-03-25 22:52:32 +0800

    stock

--- goprj_spider/src/main/crawler.go
+++ goprj_spider/src/main/crawler.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	resp, err := http.Get("http://hq.sinajs.cn/list=s_sz002230")
-	//	resp, err := http.Get("http://hq.sinajs.cn/list=sh002230")
+	resp, err := http.Get("http://hq.sinajs.cn/list=s_sz002230,s_sh600000")
+	// resp, err := http.Get("http://hq.sinajs.cn/list=sz002230,sh600000")
 	if err != nil {
 		fmt.Println("http get error", err)
 		return
@@ -19,8 +22,76 @@ func main() {
 		fmt.Println("read error", err)
 		return
 	}
-	b, err := simplifiedchinese.GBK.NewEncoder().Bytes([]byte(str))
-	fmt.Println(string(b))
+	//fmt.Println(typeof(body))
+	//fmt.Println(string(body))
+	utf8Bytes, err := GbkToUtf8(body)
+	if err != nil {
+		fmt.Println("GbkToUtf8 error", err)
+		return
+	}
+	// fmt.Println(string(utf8Bytes))
+	str := string(utf8Bytes)
+
+	items := strings.Split(str, "\n")
+	fmt.Println("items:", len(items))
+	for i := 0; i < len(items); i++ {
+		dataStr := items[i]
+
+		if len(strings.TrimSpace(dataStr)) == 0 {
+			continue
+		}
+		fmt.Println(dataStr)
+
+		idxStart := strings.Index(dataStr, "\"")
+		idxEnd := strings.LastIndex(dataStr, "\"")
+
+		// fmt.Println(idxStart, idxEnd)
+		fmt.Println(dataStr[idxStart+1 : idxEnd])
+	}
+}
+
+// transform GBK bytes to UTF-8 bytes
+func GbkToUtf8(str []byte) (b []byte, err error) {
+	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewDecoder())
+	b, err = ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// transform UTF-8 bytes to GBK bytes
+func Utf8ToGbk(str []byte) (b []byte, err error) {
+	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewEncoder())
+	b, err = ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// transform GBK string to UTF-8 string and replace it, if transformed success, returned nil error, or died by error message
+func StrToUtf8(str *string) error {
+	b, err := GbkToUtf8([]byte(*str))
+	if err != nil {
+		return err
+	}
+	*str = string(b)
+	return nil
+}
+
+// transform UTF-8 string to GBK string and replace it, if transformed success, returned nil error, or died by error message
+func StrToGBK(str *string) error {
+	b, err := Utf8ToGbk([]byte(*str))
+	if err != nil {
+		return err
+	}
+	*str = string(b)
+	return nil
+}
+
+func typeof(v interface{}) string {
+	return fmt.Sprintf("%T", v)
 }
 
 //import (
