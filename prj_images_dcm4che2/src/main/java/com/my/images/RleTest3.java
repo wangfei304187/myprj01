decc996d3dd526361632fd36fd8981336eeed4f0
wf <wf@promote.cache-dns.local>
2020-06-22 16:36:28 +0800

    new file:   prj_images_dcm4che2/note.txt 	new file:   prj_images_dcm4che2/src/main/java/com/my/images/RleTest3.java 	new file:   prj_images_dcm4che2/testdata.txt

---
+++ prj_images_dcm4che2/src/main/java/com/my/images/RleTest3.java
@@ -0,0 +1,246 @@
+package com.my.images;
+
+public class RleTest3
+{
+    public static void main(String[] args)
+    {
+        /*@formatter:off*/
+        byte[][] in = new byte[][]{
+                //                new byte[] { 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C', 'C', 'C' },
+                //
+                //                new byte[] { 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C', 'C'},
+                //                new byte[] { 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C'},
+                //                new byte[] { 'A', 'A', 'A', 'B', 'B', 'C'},
+                //                new byte[] { 'A', 'A', 'A', 'B', 'C'},
+                //                new byte[] { 'A', 'A', 'B', 'C'},
+                //
+                //                new byte[] { 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C', 'C', 'C' },
+                //                new byte[] { 'A', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'C', 'C', 'C' },
+                //                new byte[] { 'A', 'B', 'B', 'C', 'C', 'C' },
+                //                new byte[] { 'A', 'B', 'C', 'C', 'C' },
+                //                new byte[] { 'A', 'B', 'C', 'C' },
+                //
+                //                new byte[] { 'A', 'B', 'C'},
+                //                new byte[] { 'A', 'B' }
+                //, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+                //, new byte[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -68, -68, 0, 0, 0, 0, -68, -68, 0, 0, -68, -68, -68, -68, -68, -68, -68, 0, 0, 0, 0}
+                //,
+                new byte[] {
+                        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+                        ,
+                        0,0,0,-68,-68,0,0,0,0,-68,-68,0,0,-68,-68,-68,-68,-68,-68,-68,0,0,0,0,-68,-68,-68,-68,-68,0,0,0,0,-68,-68,-68,-68,-68,-68,-68,-68,0,0,-68,0,0,0,0,0,0,-68,0,0,0,0,-68,-68,-68,0,0,0,0,0,-68,-68,0,0,0,-68,-68,0,0,0,-68,-68,0,0,0,-68,-68,0,0,0,-68,-68,-68,-68,-68,-68,-68,-68,0,0,0,0,0,0,0,0,0,0,0,0,0,-68,-68,-68,-68,-68,0,0,0,0,0,0,-68,-68,-68,0,0,0,0,0,-68,-68,0,0,0,-68,-68,0,0,0,0,0,-68,-68,-68,0,0,0,0,0,-68,-68,-68,-68,-68,0,0,0,0,0,-68,-68,-68,-68,-68,-68,-68,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-68,-68,-68,-68,-68,-68,0,0,0,0,-68,-68,-68,-68,-68,-68,-68,0,0,0,-68,-68,0,0,0,-68,-68,0,0,0,0,0,-68,-68,-68,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-68,-68,0,0,0,0,-68,-68,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-88,-88,-88,0,0,0,0,0,0,-88,-88,0,0,0,0,0,0,0,0,0,0,0,0,-88,-88,-88,-88,0,0,0,0,0,-88,-88,0,0,0,0,0,-88,-88,0,0,0,0,0,0,-88,-88,-88,0,0,0,0,0,0,0,0,0,0,0
+                }
+                //, new byte[] { 'A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'B' }
+
+        };
+        /*@formatter:on*/
+
+        for (int i = 0; i < in.length; i++)
+        {
+            System.out.println("Case " + i + ", input len=" + in[i].length);
+            RleTest3.doTest(in[i]);
+
+            if (i % 5 == 0)
+            {
+                System.out.println();
+            }
+        }
+
+    }
+
+    public static void doTest(byte[] in)
+    {
+        byte[] out = new byte[10000];
+        RleTest3.rle(out, in, 608);
+        for (int i = 0; i < out.length; i++)
+        {
+            System.out.print(out[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void rle(byte[] out, byte[] inputBytes, int sizePerConversion)
+    {
+        int outStartOffset = 0;
+        int inStartOffset = 0;
+        while (true)
+        {
+            if (inStartOffset + sizePerConversion < inputBytes.length)
+            {
+                System.out.println("*** outStartOffset=" + outStartOffset + ", inStartOffset=" + inStartOffset
+                        + ", sizePerConversion=" + sizePerConversion + ", inputBytes.length=" + inputBytes.length);
+
+                outStartOffset = RleTest3.rle(out, outStartOffset, inputBytes, inStartOffset, sizePerConversion);
+                inStartOffset += sizePerConversion;
+            }
+            else if (inStartOffset + sizePerConversion == inputBytes.length)
+            {
+                System.out.println("*** outStartOffset=" + outStartOffset + ", inStartOffset=" + inStartOffset
+                        + ", sizePerConversion=" + sizePerConversion + ", inputBytes.length=" + inputBytes.length);
+
+                outStartOffset = RleTest3.rle(out, outStartOffset, inputBytes, inStartOffset, sizePerConversion);
+                inStartOffset += sizePerConversion;
+
+                break;
+            }
+            else
+            {
+                sizePerConversion = inputBytes.length - inStartOffset;
+
+                System.out.println("*** outStartOffset=" + outStartOffset + ", inStartOffset=" + inStartOffset
+                        + ", sizePerConversion=" + sizePerConversion + ", inputBytes.length=" + inputBytes.length);
+
+                outStartOffset = RleTest3.rle(out, outStartOffset, inputBytes, inStartOffset, sizePerConversion);
+                inStartOffset += sizePerConversion;
+                break;
+            }
+        }
+    }
+
+    public static int rle(byte[] out, int outStartOffset, byte[] inputBytes, int inStartOffset, int sizePerConversion)
+    {
+        int LIMIT = 128;
+        // 0 -- INIT, 1 -- LITERAL RUN, 2 -- REPLICATE RUN
+        int MODE_INIT = 0;
+        int MODE_LITERAL = 1;
+        int MODE_REPLICATE = 2;
+
+        int curMode = MODE_INIT;
+
+        int outCntIdx = 0 + outStartOffset;
+        int outPos = outCntIdx + 1;
+
+        int inPos = 0 + inStartOffset;
+        int inPosNext = inPos + 1;
+
+        int count = 0;
+        int count1 = 1;
+
+        // int inputEnd = inputBytes.length;
+        int inputEnd = inStartOffset + sizePerConversion;
+
+        while (inPos < inputEnd)
+        {
+            byte curr = inputBytes[inPos];
+            byte next;
+            if (inPosNext < inputEnd)
+            {
+                next = inputBytes[inPosNext];
+            }
+            else
+            {
+                if (curMode == MODE_INIT)
+                {
+                    out[outCntIdx] = 0;
+                }
+                else if (curMode == MODE_LITERAL)
+                {
+                    out[outCntIdx]++;
+                }
+                else
+                {
+                    // do nothing
+                }
+
+                out[outPos] = curr;
+
+                outPos++;
+                outCntIdx = outPos;
+                outPos = outCntIdx + 1;
+                count = 0;
+                count1 = 1;
+                break;
+            }
+
+            if (next != curr) // MODE_LITERAL
+            {
+                if (curMode == MODE_REPLICATE) // 2 -- REPLICATE RUN
+                {
+                    inPos++;
+                    inPosNext = inPos + 1;
+
+                    outCntIdx += 2;
+                    outPos = outCntIdx + 1;
+                    count = 0;
+                    count1 = 1;
+
+                    curMode = MODE_INIT;
+                    continue;
+                }
+
+                curMode = MODE_LITERAL;
+
+                if (count + 1 <= LIMIT)
+                {
+                    count++;
+
+                    out[outCntIdx] = (byte) (count - 1);
+                    out[outPos] = inputBytes[inPos];
+                    outPos++;
+
+                    inPos++;
+                    inPosNext++;
+                }
+                else
+                {
+                    outCntIdx += count + 1;
+                    outPos = outCntIdx + 1;
+                    count = 0;
+                    count1 = 1;
+
+                    curMode = MODE_INIT;
+                    continue;
+                }
+            }
+            else
+            { // Case2: next == curr, MODE_REPLICATE
+
+                if (curMode == MODE_LITERAL) // 1 -- LITERAL RUN
+                {
+                    // inPos++;
+
+                    outCntIdx = outPos;
+                    outPos = outCntIdx + 1;
+                    count = 0;
+                    count1 = 1;
+
+                    curMode = MODE_INIT;
+                    continue;
+                }
+
+                curMode = MODE_REPLICATE;
+
+                if (count1 + 1 <= LIMIT)
+                {
+                    // count++;
+
+                    out[outCntIdx] = (byte) (-(count1 + 1) + 1);
+                    out[outPos] = inputBytes[inPos];
+
+                    count1++;
+
+                    inPos++;
+                    inPosNext++;
+                }
+                else
+                {
+                    outCntIdx += 2;
+                    outPos = outCntIdx + 1;
+                    count = 0;
+                    count1 = 1;
+
+                    inPos++;
+                    inPosNext++;
+
+                    curMode = MODE_INIT;
+                    continue;
+                }
+            }
+
+        } // end while
+
+        return outCntIdx;
+
+    } // end rle
+
+}
