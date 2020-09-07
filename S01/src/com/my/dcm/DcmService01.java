a52ffcc94efcb27ce181a51de7b4dcb06250c0da
wangfei304187 <george_7018@163.com>
2020-09-07 23:03:11 +0800

    modified:   S01/pom.xml 	modified:   S01/src/com/my/dcm/DcmService01.java 	S01/.classpath 	S01/.project 	S01/.settings/ 	S01/note.txt 	S01/resources/ 	S01/src/com/my/dcm/StudyController.java

--- S01/src/com/my/dcm/DcmService01.java
+++ S01/src/com/my/dcm/DcmService01.java
@@ -1,5 +1,9 @@
 package com.my.dcm;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DcmService01 {
+public class DcmService01 implements ApplicationRunner{
 
+	
+	private static final Logger logger = LoggerFactory.getLogger(DcmService01.class);
+	
     public static void main(String[] args) {
+    	logger.info("BEFORE SpringApplication.run");
+    	
         SpringApplication.run(DcmService01.class, args);
+        
+        logger.info("AFTER SpringApplication.run");
 
         String applicationPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         System.out.println(applicationPath);
@@ -21,4 +32,33 @@ public class DcmService01 {
     public String hello(@RequestParam(value = "name", defaultValue = "DICOM") String name) {
         return String.format("Hello %s!", name);
     }
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		System.out.println("DcmService01::run");
+	}
+    
+//    @Bean
+//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+////    	return args -> {
+////    		String[] beanNames = ctx.getBeanDefinitionNames();
+////    		Arrays.sort(beanNames);
+////    		for(String beanName: beanNames)
+////    		{
+////    			System.out.println(beanName);
+////    		}
+////    	};
+//    	
+//    	return new CommandLineRunner() {
+//			@Override
+//			public void run(String... args) throws Exception {
+//	    		String[] beanNames = ctx.getBeanDefinitionNames();
+//	    		// Arrays.sort(beanNames);
+//	    		for(String beanName: beanNames)
+//	    		{
+//	    			System.out.println(beanName);
+//	    		}
+//			}
+//		};
+//    }
 }
