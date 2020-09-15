9c7affe0468dc82aa36dbc9039f3ce94de1aaeec
wf <wf@localhost.localdomain>
2020-09-15 16:44:09 +0800

    modified:   S01/S01.iml 	new file:   S02/S02.iml 	modified:   S02/src/com/test/demo/DemoApplication.java

--- S02/src/com/test/demo/DemoApplication.java
+++ S02/src/com/test/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
