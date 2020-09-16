001ac5493d4cf208212023971cc749cd5015464e
wf <wf@localhost.localdomain>
2020-09-16 23:22:50 +0800

    jwt

--- S01/src/com/my/scheduler/Scheduler.java
+++ S01/src/com/my/scheduler/Scheduler.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@EnableScheduling
+//@EnableScheduling
 @Component
 public class Scheduler {
 
