d76ece999a05eb931e32930143de70d03898e73f
wf <wf@localhost.localdomain>
2020-09-22 17:06:18 +0800

    up

--- S04/src/main/java/com/my/app/controller/WebController.java
+++ S04/src/main/java/com/my/app/controller/WebController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-	@RequestMapping(value = "/showStart")
+	@RequestMapping(value = "/start")
 	public String start() {
 		return "start";
 	}
