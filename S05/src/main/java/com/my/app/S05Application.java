13fa9ea3720839e2e5889f176bc4f3222ae958ec
wf <wf@localhost.localdomain>
2020-09-22 17:02:40 +0800

    add S05

---
+++ S05/src/main/java/com/my/app/S05Application.java
@@ -0,0 +1,13 @@
+package com.my.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S05Application.class, args);
+    }
+
+}
