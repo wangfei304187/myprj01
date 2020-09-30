c4517885ade8d5855e43b1f830cc56244b22659b
wf <wf@localhost.localdomain>
2020-09-30 13:25:22 +0800

    test Bcrypt

--- prj_images_dcm4che2/src/test/java/com/my/images/AppTest.java
+++ prj_images_dcm4che2/src/test/java/com/my/images/AppTest.java
@@ -1,38 +1,37 @@
 package com.my.images;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    /**
-     * Rigourous Test :-)
-     */
     public void testApp()
     {
         assertTrue( true );
+    }
+    
+    public void testBCrypt()
+    {
+    	String salt = BCrypt.gensalt();
+    	System.out.println("salt=" + salt);
+    	String pw_hash = BCrypt.hashpw("abcd", salt);
+    	System.out.println("pw_hash=" + pw_hash);
+    	
+    	boolean r = BCrypt.checkpw("abcd", pw_hash);
+    	System.out.println("r=" + r);
     }
 }
