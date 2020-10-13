85e440689818835f6070d73661cda20da68cb671
wf <wf@localhost.localdomain>
2020-10-13 15:36:13 +0800

    produceDp

--- prj_images_dcm4che2/src/test/java/com/my/images/AppTest.java
+++ prj_images_dcm4che2/src/test/java/com/my/images/AppTest.java
@@ -6,6 +6,9 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AppTest 
     extends TestCase
 {
@@ -33,5 +36,45 @@ public class AppTest
     	
     	boolean r = BCrypt.checkpw("abcd", pw_hash);
     	System.out.println("r=" + r);
+    }
+
+    public void testC()
+    {
+        int[] cc = new int[] {1, 3, 5, 7};
+        int total = 14;
+
+        int[] dp = produceDp(cc, total);
+
+        System.out.println("out[" + total + "]=" + dp[total]);
+    }
+
+    private int[] produceDp(int[] cc, int total) {
+        int[] dp = new int[total + 1];
+
+        for(int i=0; i<dp.length; i++) {
+            dp[i] = -1;
+        }
+
+        for(int i=0; i<dp.length; i++) {
+
+            if (i == 0) {
+                dp[i] = 0;
+                continue;
+            }
+
+            int min = Integer.MAX_VALUE;
+            for(int j=0; j<cc.length; j++) {
+                int c = cc[j];
+                if (c <= i) {
+                    int tmp = dp[i-c] + 1;
+                    if (tmp < min)
+                    {
+                        min = tmp;
+                    }
+                }
+            }
+            dp[i] = min;
+        }
+        return dp;
     }
 }
