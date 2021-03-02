bb12056bcc85e38bae20fb76b890eef1f1d4726e
wf <wf@localhost.localdomain>
2021-03-02 16:10:19 +0800

    clean

--- prj_lucene/src/main/java/com/demo/lucene/LuceneIndexer.java
+++ prj_lucene/src/main/java/com/demo/lucene/LuceneIndexer.java
@@ -1,6 +1,5 @@
 package com.demo.lucene;
 
-
 import java.io.IOException;
 import java.nio.file.Paths;
 
@@ -16,47 +15,45 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
-
 /**
  * <pre>
- *  Lucene创建索引服务类
+ * Lucene创建索引服务类
  * </pre>
  *
  * @author nicky
  * @version 1.00.00
- *
- * <pre>
- * 修改记录
- *    修改后版本:     修改人：  修改日期:2018年04月18日     修改内容:
- *          </pre>
  */
-public class LuceneIndexer {
+public class LuceneIndexer
+{
 
     private volatile static LuceneIndexer instance;
 
     private final static String INDEX_DIR = "xxx.index";
 
-    private static class SingletonHolder {
+    private static class SingletonHolder
+    {
         private final static LuceneIndexer instance = new LuceneIndexer();
     }
 
-    public static LuceneIndexer getInstance() {
+    public static LuceneIndexer getInstance()
+    {
         return SingletonHolder.instance;
     }
 
-    public boolean createIndex(String indexDir) throws IOException {
-        //加点测试的静态数据
-        Integer ids[] = {1, 2, 3};
-        String titles[] = {"标题3", "标题2", "谷歌地图之父跳槽facebook"};
+    public boolean createIndex(String indexDir) throws IOException
+    {
+        // 加点测试的静态数据
+        Integer ids[] = { 1, 2, 3 };
+        String titles[] = { "标题3", "标题2", "谷歌地图之父跳槽facebook" };
         String tcontents[] = {
                 "内容1内容啊哈哈哈A",
                 "内容2内容啊哈哈哈B",
                 "啊哈哈哈C"
         };
 
-        long startTime = System.currentTimeMillis();//记录索引开始时间
+        long startTime = System.currentTimeMillis();// 记录索引开始时间
 
-//        Analyzer analyzer = new SmartChineseAnalyzer();
+        // Analyzer analyzer = new SmartChineseAnalyzer();
         Analyzer analyzer = new IKAnalyzer();
         Directory directory = FSDirectory.open(Paths.get(indexDir));
         IndexWriterConfig config = new IndexWriterConfig(analyzer);
@@ -64,35 +61,43 @@ public class LuceneIndexer {
 
         IndexWriter indexWriter = new IndexWriter(directory, config);
 
-        for (int i = 0; i < ids.length; i++) {
+        for (int i = 0; i < ids.length; i++)
+        {
             Document doc = new Document();
-            //添加字段
-//            doc.add(new IntField("id", ids[i], Field.Store.YES)); //添加内容 // 5.3.1
-            doc.add(new IntPoint("id", ids[i])); //添加内容 // 8.8.1
-            doc.add(new TextField("title", titles[i], Field.Store.YES)); //添加文件名，并把这个字段存到索引文件里
-            doc.add(new TextField("tcontent", tcontents[i], Field.Store.YES)); //添加文件路径
+            // 添加字段
+            // doc.add(new IntField("id", ids[i], Field.Store.YES)); //添加内容 // 5.3.1
+            doc.add(new IntPoint("id", ids[i])); // 添加内容 // 8.8.1
+            doc.add(new TextField("title", titles[i], Field.Store.YES)); // 添加文件名，并把这个字段存到索引文件里
+            doc.add(new TextField("tcontent", tcontents[i], Field.Store.YES)); // 添加文件路径
             indexWriter.addDocument(doc);
             System.out.println("add doc " + i);
         }
 
         indexWriter.commit();
-//        System.out.println("共索引了" + indexWriter.numDocs() + "个文件");
-//        System.out.println("共索引了" + indexWriter.getMaxCompletedSequenceNumber() + "个文件");
+        // System.out.println("共索引了" + indexWriter.numDocs() + "个文件");
+        // System.out.println("共索引了" + indexWriter.getMaxCompletedSequenceNumber() + "个文件");
         indexWriter.close();
         System.out.println("创建索引所用时间：" + (System.currentTimeMillis() - startTime) + "毫秒");
 
         return true;
     }
 
-    public static void main(String[] args) {
-        try {
-            boolean r = LuceneIndexer.getInstance().createIndex(INDEX_DIR);
-            if (r) {
+    public static void main(String[] args)
+    {
+        try
+        {
+            boolean r = LuceneIndexer.getInstance().createIndex(LuceneIndexer.INDEX_DIR);
+            if (r)
+            {
                 System.out.println("索引创建成功!");
-            } else {
+            }
+            else
+            {
                 System.out.println("索引创建失败!");
             }
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
 
--- prj_lucene/src/main/java/com/demo/lucene/SearchBuilder.java
+++ prj_lucene/src/main/java/com/demo/lucene/SearchBuilder.java
@@ -1,78 +1,98 @@
 package com.demo.lucene;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.highlight.*;
+import org.apache.lucene.search.highlight.Fragmenter;
+import org.apache.lucene.search.highlight.Highlighter;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+import org.apache.lucene.search.highlight.QueryScorer;
+import org.apache.lucene.search.highlight.SimpleHTMLFormatter;
+import org.apache.lucene.search.highlight.SimpleSpanFragmenter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.nio.file.Paths;
+public class SearchBuilder
+{
 
+    public static void doSearch(String indexDir, String queryStr) throws IOException, ParseException, InvalidTokenOffsetsException
+    {
+        Path p = Paths.get(indexDir);
+        System.out.println(p.toString());
 
-/**
- * <pre>
- *  Lucene全局搜索服务类
- * </pre>
- *
- * @author nicky
- * @version 1.00.00
- *
- * <pre>
- * 修改记录
- *    修改后版本:     修改人：  修改日期:2018年04月18日     修改内容:
- *          </pre>
- */
-public class SearchBuilder {
-
-    public static void doSearch(String indexDir, String queryStr) throws IOException, ParseException, InvalidTokenOffsetsException {
-        Directory directory = FSDirectory.open(Paths.get(indexDir));
+        Directory directory = FSDirectory.open(p);
         DirectoryReader reader = DirectoryReader.open(directory);
         IndexSearcher searcher = new IndexSearcher(reader);
-        Analyzer analyzer = new SmartChineseAnalyzer();
-        QueryParser parser = new QueryParser("tcontent", analyzer);
+
+        // Analyzer analyzer = new SmartChineseAnalyzer();
+        Analyzer analyzer = new IKAnalyzer();
+        // QueryParser parser = new QueryParser("tcontent", analyzer);
+        QueryParser parser = new MultiFieldQueryParser(new String[] { "title", "tcontent" }, analyzer);
+        parser.setPhraseSlop(3);
+        // 设置短语搜索的坡度为3,默认为0
+        parser.setDefaultOperator(QueryParser.Operator.AND);
         Query query = parser.parse(queryStr);
 
         long startTime = System.currentTimeMillis();
         TopDocs docs = searcher.search(query, 10);
 
-        System.out.println("查找" + queryStr + "所用时间：" + (System.currentTimeMillis() - startTime));
-        System.out.println("查询到" + docs.totalHits + "条记录");
+        System.out.println("查找'" + queryStr + "'所用时间：" + (System.currentTimeMillis() - startTime));
+        System.out.println("查询到'" + docs.totalHits + "'条记录");
 
+        // 加入高亮显示的
         SimpleHTMLFormatter simpleHTMLFormatter = new SimpleHTMLFormatter("<b><font color=red>", "</font></b>");
-        QueryScorer scorer = new QueryScorer(query);//计算查询结果最高的得分
-        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);//根据得分算出一个片段
+        QueryScorer scorer = new QueryScorer(query);// 计算查询结果最高的得分
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer);// 根据得分算出一个片段
         Highlighter highlighter = new Highlighter(simpleHTMLFormatter, scorer);
+        highlighter.setTextFragmenter(fragmenter);// 设置显示高亮的片段
 
-        //遍历查询结果
-        for (ScoreDoc scoreDoc : docs.scoreDocs) {
+        // 遍历查询结果
+        for (ScoreDoc scoreDoc : docs.scoreDocs)
+        {
             Document doc = searcher.doc(scoreDoc.doc);
             String tcontent = doc.get("tcontent");
-            if (tcontent != null) {
+            if (tcontent != null)
+            {
                 TokenStream tokenStream = analyzer.tokenStream("tcontent", new StringReader(tcontent));
                 String summary = highlighter.getBestFragment(tokenStream, tcontent);
+                System.out.println(summary);
+            }
+
+            String title = doc.get("title");
+            if (title != null)
+            {
+                TokenStream tokenStream = analyzer.tokenStream("title", new StringReader(title));
+                String summary = highlighter.getBestFragment(tokenStream, title);
                 System.out.println(summary);
             }
         }
         reader.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String indexDir = "xxx.index";
-        String q = "内容"; //查询这个字符串
-        try {
-            doSearch(indexDir, q);
-        } catch (Exception e) {
+        String q = "内容"; // 查询这个字符串
+        try
+        {
+            SearchBuilder.doSearch(indexDir, q);
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
     }
